92b8a1c65706b6ac189c90e6847cc3bafbb68c0d
= <=>
2022-04-13 15:08:02 +0300

    frontend adding

---
+++ frontend/frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>
+    <router-link to="/">All Categories</router-link> |
+    <router-link to="/home">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+
+@import './assets/stylesheet.scss'
+</style>
---
+++ frontend/frontend/src/components/posts.vue
@@ -0,0 +1,33 @@
+<template>
+  <div type="button">
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'SingleCategory',
+  data () {
+    return {
+      data: []
+    }
+  },
+  methods: {
+    getReponse () {
+      const path = 'http://localhost:5000/categories'
+      axios.get(path + '/' + this.$route.params.categoryname + '/' + this.$route.params.blogID)
+        .then(function (response) {
+          console.log(response.data)
+          this.data = response.data
+        }.bind(this))
+        .catch((err) => {
+          console.error(err)
+        })
+    }
+  },
+  created () {
+    this.getReponse()
+  }
+}
+</script>
